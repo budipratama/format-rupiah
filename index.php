835131987b7aa1892ec8364c0abd8a59f5e400f1
irezpratama <boedipratama19@gmail.com>
2020-04-02 02:14:32 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+
+use budipratama\formatrupiah\FormatRupiah;
+
+echo "mausk";
+//die();
+//use formatrupiah\FormatRupiah;
+$user = new FormatRupiah;
+//$user->index();
---
+++ src/FormatRupiah.php
@@ -0,0 +1,11 @@
+<?php
+namespace budipratama\formatrupiah;
+echo "class format rupiah";die();
+class FormatRupiah
+{
+    public function index()
+    {
+        echo "Ini adalah output dari class UserController method index \n";
+    }
+}
+
